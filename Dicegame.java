43e5ff47b05323992ade13a0379f093c12cf3f1a
min-zemi <pengbianquanji@gmail.com>
2025-06-27 15:06:10 +0900

    modification

--- Dicegame.java
+++ Dicegame.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class DiceGame{
+public class Dicegame{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         Random dice = new Random();
